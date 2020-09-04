2c2f6430dd7e2bc8ce766d53c2f0fa4edd39ea4d
osagiestar <okoedooc@yahoo.com>
2020-09-04 10:06:13 +0100

    working on x of 73 episodes message

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const container = document.getElementById("container");
-  // const rootElem = document.getElementById("root"); 
   
   // creates a div for every episode and adds the info or details //  
   episodeList.forEach((episode) => {
@@ -19,61 +18,37 @@ function makePageForEpisodes(episodeList) {
 </div>`;
   
 // function adds "0" to number to give it a double digit // 
-function zeroPadded(episodeCode) {
+  function zeroPadded(episodeCode) {
   return episodeCode.toString().padStart(2, 0); 
-}
-})
+  }
+  })
 };
 
 // let episodesAdd;
   
 document.addEventListener("keyup", () => {
   let episodesAll = document.querySelectorAll("#container div");
-  // console.log(episodeAll);
-  // const allEpisodes = getAllEpisodes();
- 
-   let searchInput = document.getElementById("searchInput").value;
-   
+  let searchInput = document.getElementById("searchInput").value;
+
   let searchResult = document.getElementById("searchResult");
-  // searchResult = [];
+  
   console.log(searchInput);
   episodesAll.forEach((episode) => {
-    console.log(episode.innerHTML);
+  
     if (episode.innerHTML.toLowerCase().includes(searchInput.toLowerCase()))
     // episode.name.toUpperCase().includes(searchInput.toUpperCase())) 
     // || episode.summary.toUpperCase().includes(searchInput.toUpperCase())) 
     {
       episode.style.display = "";
+      console.log(episode.style.display = "");
     } else {
-      episode.style.display = "none"};
-})
+      episode.style.display = "none";
+    }; 
+    searchResult.innerHTML = `${episodesAll.length} of ${episodesAll.length} episodes selected`;
+  })
+  
+ 
 });
 // console.log(episodesAdd);
 
   window.onload = setup;
-
-
-// let episodeVar = getAllEpisodes()
-    // // filterResult = episodeList.filter(episode) 
-    // searchFilter = episodeVar.filter() {
-    //    console.log(episodeVar);
-    // };
-   
-    // if (episode.name || episode.summary) {
-    //   searchResult.style.display = ""
-    // } else {
-    //   searchResult.style.display = "none";
-    // 
-
-// {
-//   searchResult.innerHTML =
-//   `<div>
-//     <h2>${episode.name} - S${zeroPadded(episode.season)} 
-//     E${zeroPadded(episode.number)} </h2>
-//     <img src= "${episode.image.medium}" alt "">
-//     ${episode.summary}
-//     </div>`
-// }
-// {
-//   episode.style.display = "none";
-// }

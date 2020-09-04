b180cc6bdef15e5ebc73b506c28ebe7bb6efcdbd
osagiestar <okoedooc@yahoo.com>
2020-09-04 11:32:30 +0100

    editted code

--- script.js
+++ script.js
@@ -2,26 +2,30 @@
 
 function setup() {
   const allEpisodes = getAllEpisodes();
-  makePageForEpisodes(allEpisodes);
-}
+  makePageForEpisodes(allEpisodes); 
+  console.log(allEpisodes)
+};
 
 function makePageForEpisodes(episodeList) {
   const container = document.getElementById("container");
-  
+  let searchResult = document.getElementById("searchResult");
+  let allEpisodes;
   // creates a div for every episode and adds the info or details //  
   episodeList.forEach((episode) => {
+    
     container.innerHTML += `<div>
     <h2>${episode.name} - S${zeroPadded(episode.season)} 
     E${zeroPadded(episode.number)} </h2>
     <img src= "${episode.image.medium}" alt "">
     ${episode.summary}
-</div>`;
-  
-// function adds "0" to number to give it a double digit // 
-  function zeroPadded(episodeCode) {
-  return episodeCode.toString().padStart(2, 0); 
-  }
+    </div>`;
   })
+  searchResult.innerHTML = `${episodeList.length} of ${allEpisodes.length} episodes selected`;
+};
+
+// function adds "0" to number to give it a double digit // 
+function zeroPadded(episodeCode) {
+  return episodeCode.toString().padStart(2, 0);
 };
 
 // let episodesAdd;
@@ -30,8 +34,6 @@ document.addEventListener("keyup", () => {
   let episodesAll = document.querySelectorAll("#container div");
   let searchInput = document.getElementById("searchInput").value;
 
-  let searchResult = document.getElementById("searchResult");
-  
   console.log(searchInput);
   episodesAll.forEach((episode) => {
   
@@ -44,10 +46,9 @@ document.addEventListener("keyup", () => {
     } else {
       episode.style.display = "none";
     }; 
-    searchResult.innerHTML = `${episodesAll.length} of ${episodesAll.length} episodes selected`;
+    
   })
-  
- 
+   
 });
 // console.log(episodesAdd);
 

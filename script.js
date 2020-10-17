0f4b954123c2b25f7d54a9b5d249298a1ad6f1b5
osagiestar <okoedooc@yahoo.com>
2020-10-17 19:26:31 +0100

    Level 500 completed

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 // selectors and global variables
 let dropDown = document.getElementById("listEpisodes");
+const root = document.getElementById("root");
 const container = document.getElementById("container");
 const option = document.createElement("option");
 let searchInput = document.getElementById("searchInput");
@@ -22,42 +23,62 @@ function setup() {
   });
 
   // creates show DropDown Options
+  showCount = 0;
   createShowList = allShows.forEach((show) => {
     showDropDown.innerHTML += `
     <option  value= "${show.id}">
     ${show.name}
     </option>
    `;
+    showCount++;
   });
-  //  episodeSet(allShows[0].id); // function call to load episodes //
 
+  //  episodeSet(allShows[0].id); // function call to load episodes //
+ 
   makePageForShow(allShows); // function call to load all shows //
+
+  searchResult.innerHTML = `${showCount} of ${showCount} shows available`;
 }
 
-// function that creates div for shows and displays details
+// function that creates div for shows and displays details in the root DOM
 function makePageForShow(allShows) {
-  allShows.forEach((show) => { 
-  let showEl = document.createElement("div");
-  container.appendChild(showEl);
- 
-  show.image ? (showEl.innerHTML += `
-    <h3>${show.name}</h3>
-    <img src= "${show.image.medium}" alt ""> 
-    ${show.summary ? show.summary : ""}
+    container.style.display = "none";
+    container.innerHTML = "none";
+    root.style.border = "3px solid green";
+    allShows.forEach((show) => {
+    let showEl = document.createElement("div");
+    showEl.class = "show-elm";
+    root.appendChild(showEl);
+  
+    show.image
+      ? (showEl.innerHTML += `
+     
+        <h3>${show.name}</h3>
+      <div class="show-info">
+        <img src= "${show.image.medium}" alt= ""> 
+        ${show.summary ? show.summary : ""}
+        <div id="more-details">
+          <div> Rated: ${show.rating.average} </div>
+          <div> Genre: ${show.genres} </div>
+          <div> Status: ${show.status} </div>
+          <div> Runtime: ${show.runtime} </div>
+        </div>
+      </div>
+    
     `)
-    : (showEl.innerHTML += `
+      : (showEl.innerHTML += `
     <h3>${show.name}</h3>
     ${show.summary ? show.summary : ""}`);
-    console.log(showEl);   
-})
+    console.log(showEl);
+  });
 }
 
-// function that fetches API and displays episodes 
+// function that fetches API and displays episodes
 function episodeSet(selectedShow) {
-   console.log(selectedShow);
+  console.log(selectedShow);
 
-    // API Fetch for show episodes //
-    fetch(`https://api.tvmaze.com/shows/${selectedShow}/episodes`)
+  // API Fetch for show episodes //
+  fetch(`https://api.tvmaze.com/shows/${selectedShow}/episodes`)
     .then((response) => response.json())
     .then((data) => {
       makePageForEpisodes(data);
@@ -65,23 +86,36 @@ function episodeSet(selectedShow) {
     })
     .catch((error) => console.log(error));
 }
-// eventListener for the show Dropdown
-showDropDown.addEventListener("change", (event) =>  episodeSet(event.target.value));
 
-// Level 100: function that displays episodes on webpage for GOT show//
-function makePageForEpisodes(episodeList) { 
+// eventListener for the show Dropdown
+showDropDown.addEventListener("change", (event) =>{
+  if(event.target.value !== "text"){episodeSet(event.target.value)}
+  else{root.style.display = ""; container.style.display="none"; }
+}
+
+);
+
+// Level 100: function that displays episodes on webpage for GOT show in the Container DOM//
+function makePageForEpisodes(episodeList) {
+  root.style.display = "none";
+  root.style.border = "3px solid green";
+  container.style.display ="";
   searchResult.innerHTML = "";
   container.innerHTML = "";
   dropDown.innerHTML = "";
   option.value = "";
   option.innerHTML = "Show All Episodes";
   dropDown.appendChild(option);
- 
+
   // creates Episode Codes and the Names (Headings) for the DropDown Options //
   episodeList.forEach((episode) => {
+    console.log(episodeList);
     let option = document.createElement("option");
+
+    // creates DropDown Options for the Episodes
     option.value = `${episode.name} - S${zeroPadded(episode.season)}
     E${zeroPadded(episode.number)}`;
+
     option.innerHTML = `S${zeroPadded(episode.season)}
     E${zeroPadded(episode.number)} - ${episode.name}`;
     dropDown.appendChild(option);
@@ -117,13 +151,12 @@ searchInput.addEventListener("input", (event) => {
     if (episode.innerHTML.toLowerCase().includes(searchInput.toLowerCase())) {
       episode.style.display = "block";
       episodeCount++;
+    } else {
+      episode.style.display = "none";
     }
-    else {
-       episode.style.display = "none";
-     }
   });
   // searchResult.innerHTML = `${episodeCount} of ${episodesAll.length} episodes available`;
-    searchResult.innerHTML = `${episodeCount} of ${episodesAll.length} shows available`;
+  searchResult.innerHTML = `${episodeCount} of ${episodesAll.length} shows available`;
 });
 
 // Level 300: EventListener for the DropDown selector
@@ -147,5 +180,5 @@ dropDown.addEventListener("change", (event) => {
   searchResult.innerHTML = `${episodeCount} of ${episodesAll.length} episodes selected`;
 });
 
-// loads and displays webpage 
+// loads and displays webpage
 window.onload = setup;
